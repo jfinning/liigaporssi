a1e0965aa9ba49354d2434259615967552b2882f
user <user@ubuntu.(none)>
2014-10-18 03:37:55 -0400

    added command files

---
+++ openall.sh
@@ -0,0 +1,7 @@
+nedit lp_cron.pl &
+nedit lp_freehostia_ajax.pl &
+
+nedit table_*.txt &
+
+nedit 2014/player_list_period1_nhl.txt &
+nedit 2014/player_list_period2.txt &
---
+++ runall.sh
@@ -0,0 +1,4 @@
+./lp_cron.pl -sub sm_sarjataulukko
+./lp_cron.pl -sub sm_kokoonpanot
+./lp_cron.pl -sub nhl_sarjataulukko
+./lp_cron.pl -sub nhl_kokoonpanot
