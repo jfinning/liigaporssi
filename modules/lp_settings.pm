3b71f96315e6040d5a96c00fef9f2c056b56ad76
Jaakko Finning <jaakko.finning@gmail.com>
2016-11-09 22:05:27 +0200

    Liiga period 3 start

--- modules/lp_settings.pm
+++ modules/lp_settings.pm
@@ -45,7 +45,7 @@ sub get_default_jakso ($) {
     my $liiga = shift;
     
     if ($liiga eq "sm_liiga") {
-        $jakso = "Jakso 2";
+        $jakso = "Jakso 3";
     } elsif ($liiga eq "nhl"){
         $jakso = "Jakso 1";
 	} else {
