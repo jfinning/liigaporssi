313487b442610106ee74f0f039b7c9530abea1ea
Jaakko Finning <jaakko.finning@gmail.com>
2017-03-10 20:32:47 +0200

    NHL period 5 start

--- modules/lp_settings.pm
+++ modules/lp_settings.pm
@@ -47,7 +47,7 @@ sub get_default_jakso ($) {
     if ($liiga eq "sm_liiga") {
         $jakso = "Jakso 5";
     } elsif ($liiga eq "nhl"){
-        $jakso = "Jakso 4";
+        $jakso = "Jakso 5";
 	} else {
         $jakso = "undef";
     }
