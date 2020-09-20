3337f930f392c2c4be1b30bb798390ba2fe1b3f9
Jaakko Finning <jaakko.finning@gmail.com>
2020-09-20 16:22:53 +0300

    Liiga 2020 period 1

--- modules/lp_settings.pm
+++ modules/lp_settings.pm
@@ -5,7 +5,7 @@ sub get_vuodet ($) {
     my $liiga = shift;
     
     if ($liiga =~ /sm_liiga/) {
-        @vuodet = ("2014", "2015", "2016", "2017", "2018", "2019");
+        @vuodet = ("2014", "2015", "2016", "2017", "2018", "2019", "2020");
     } else {
         @vuodet = ("2014", "2015", "2016", "2017", "2018", "2019");
     }
@@ -27,7 +27,7 @@ sub get_default_vuosi ($) {
     my $liiga = shift;
 
     if ($liiga eq "sm_liiga") {
-        $vuosi = 2019;
+        $vuosi = 2020;
     } elsif ($liiga eq "nhl"){
         $vuosi = 2019;
 	}
@@ -43,9 +43,9 @@ sub get_default_jakso ($) {
     my $liiga = shift;
     
     if ($liiga eq "sm_liiga") {
-        $jakso = "Jakso 5";
+        $jakso = "Jakso 1";
     } elsif ($liiga eq "nhl"){
-        $jakso = "Jakso 5";
+        $jakso = "Jakso PO";
     }
     
     return $jakso;
@@ -59,8 +59,8 @@ sub get_joukkue_list ($) {
         @joukkueet = ("HIFK", "HPK", "Ilves", "Jukurit", "JYP", "KalPa", "KooKoo", "Karpat", "Lukko", "Pelicans", "SaiPa", "Sport", "Tappara", "TPS", "Assat");
         #@joukkueet = ("HPK", "Karpat");
     } else {
-        @joukkueet = ("Anaheim", "Arizona", "Boston", "Buffalo", "Calgary", "Carolina", "Chicago", "Colorado", "Columbus", "Dallas", "Detroit", "Edmonton", "Florida", "Los Angeles", "Minnesota", "Montreal", "Nashville", "New Jersey", "NY Islanders", "NY Rangers", "Ottawa", "Philadelphia", "Pittsburgh", "San Jose", "St. Louis", "Tampa Bay", "Toronto", "Vancouver", "Vegas", "Washington", "Winnipeg");
-        #@joukkueet = ("Boston", "Calgary", "Carolina", "Colorado", "Columbus", "Dallas", "Nashville", "NY Islanders", "Pittsburgh", "San Jose", "St. Louis", "Tampa Bay", "Toronto", "Vegas", "Washington", "Winnipeg");
+        #@joukkueet = ("Anaheim", "Arizona", "Boston", "Buffalo", "Calgary", "Carolina", "Chicago", "Colorado", "Columbus", "Dallas", "Detroit", "Edmonton", "Florida", "Los Angeles", "Minnesota", "Montreal", "Nashville", "New Jersey", "NY Islanders", "NY Rangers", "Ottawa", "Philadelphia", "Pittsburgh", "San Jose", "St. Louis", "Tampa Bay", "Toronto", "Vancouver", "Vegas", "Washington", "Winnipeg");
+        @joukkueet = ("Dallas", "Tampa Bay");
         #@joukkueet = ("Boston", "St. Louis");
     }
     
